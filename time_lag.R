70d91d89ae1cef1bcdaef8783dc00c172f9c272d
klapierre <lapierre.kimberly@gmail.com>
2016-11-29 15:09:52 -0800

    Added cedar creek e001 field C time lag analyses and figures.

--- time_lag.R
+++ time_lag.R
@@ -20,6 +20,7 @@ theme_update(axis.title.x=element_text(size=40, vjust=-0.35, margin=margin(t=15)
 rawAbundance <- read.csv('CORRE_irrigationL_e001D_subset.csv')%>%
   select(-X)
 
+
 #generate presence/absence data
 presenceAbsence <- rawAbundance%>%
   mutate(presence=1)%>%
@@ -188,6 +189,107 @@ print(rateChangeIntervalE001AbundFig, vp=viewport(layout.pos.row=1, layout.pos.c
 print(rateChangeIntervalE001PresFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
 print(rateChangeIntervalE001AbundFigB, vp=viewport(layout.pos.row=2, layout.pos.col=1))
 print(rateChangeIntervalE001PresFigB, vp=viewport(layout.pos.row=2, layout.pos.col=2))
+#export as 1500x1500
+
+
+
+
+
+rawAbundanceFieldC <- read.csv('CORRE_raw_abundance.csv')%>%
+  select(-X)%>%
+  filter(site_code=='CDR'&project_name=='e001'&community_type=='C')
+
+
+#generate presence/absence data
+presenceAbsenceC <- rawAbundanceFieldC%>%
+  mutate(presence=1)%>%
+  select(-abundance)%>%
+  spread(key=genus_species, value=presence, fill=0)%>%
+  gather(key=genus_species, value=presence, -site_code, -project_name, -calendar_year, -treatment_year, -block, -treatment, -plot_id, -community_type)
+
+#generate treatment list
+trt <- rawAbundanceFieldC%>%
+  select(site_code, project_name, treatment, plot_id)%>%
+  unique()%>%
+  mutate(plot_id=as.character(plot_id))
+
+#e001 field C raw abundance subset
+rawAbundanceE001C <- rawAbundanceFieldC%>%
+  filter(project_name=='e001')%>%
+  #get only the control and highest N treatments
+  filter(treatment=='9_y_n'|treatment=='8_y_n')
+
+#e001 raw abundance subset
+presenceAbsenceE001C <- presenceAbsenceC%>%
+  filter(project_name=='e001')%>%
+  #get only the control and highest N treatments
+  filter(treatment=='9_y_n'|treatment=='8_y_n')
+
+
+###calculations
+###raw abundance data
+#rate change slopes
+rateChangeCAbund <- rate_change(rawAbundanceE001C, time.var='treatment_year', species.var='genus_species', abundance.var='abundance', replicate.var='plot_id')
+
+#rate change intervals for visualization
+rateChangeIntervalCAbund <- rate_change_interval(rawAbundanceE001C, time.var='treatment_year', species.var='genus_species', abundance.var='abundance', replicate.var='plot_id')%>%
+  mutate(project_name='e001')%>%
+  left_join(trt)%>%
+  mutate(type='abundance')
+
+#rate change figure
+rateChangeIntervalCAbundFig <- ggplot(rateChangeIntervalCAbund, aes(interval, distance, linetype=plot_id)) +
+  geom_point(aes(color=treatment)) + stat_smooth(method = "loess", se = F, size = 2, aes(color=treatment)) +
+  xlab('Time Interval') +
+  ylab('Euclidean Distance') +
+  scale_linetype(guide='none') +
+  scale_color_manual(values=c('#3300FF', '#FF3333'), name='Treatment', labels=c('high NPK+', 'control')) +
+  annotate('text', x=0, y=900, label='(a) Abundance-based', size=10, hjust='left') +
+  theme(legend.position=c(0.1,0.8))
+
+rateChangeIntervalCAbundFigB <- ggplot(rateChangeIntervalCAbund, aes(interval, distance)) +
+  geom_point(aes(color=treatment)) + stat_smooth(method = "loess", se = F, size = 2, aes(color=treatment)) +
+  xlab('Time Interval') +
+  ylab('Euclidean Distance') +
+  scale_linetype(guide='none') +
+  scale_color_manual(values=c('#3300FF', '#FF3333'), name='Treatment', labels=c('high NPK+', 'control')) +
+  annotate('text', x=0, y=900, label='(c) Abundance-based', size=10, hjust='left') +
+  theme(legend.position='none')
+
+###presence/absence data
+#rate change slopes
+rateChangeCPres <- rate_change(presenceAbsenceE001C, time.var='treatment_year', species.var='genus_species', abundance.var='presence', replicate.var='plot_id')
+
+#rate change intervals for visualization
+rateChangeIntervalCPres <- rate_change_interval(presenceAbsenceE001C, time.var='treatment_year', species.var='genus_species', abundance.var='presence', replicate.var='plot_id')%>%
+  mutate(project_name='e001')%>%
+  left_join(trt)%>%
+  mutate(type='presence')
+
+#rate change figure
+rateChangeIntervalCPresFig <- ggplot(rateChangeIntervalCPres, aes(interval, distance, linetype=plot_id)) +
+  geom_point(aes(color=treatment)) + stat_smooth(method = "loess", se = F, size = 2, aes(color=treatment)) +
+  xlab('Time Interval') +
+  ylab('Euclidean Distance') +
+  scale_linetype(guide='none') +
+  scale_color_manual(values=c('#3300FF', '#FF3333'), name='Treatment', labels=c('high NPK+', 'control')) +
+  annotate('text', x=0, y=5, label='(b) Presence-based', size=10, hjust='left') +
+  theme(legend.position='none')
+
+rateChangeIntervalCPresFigB <- ggplot(rateChangeIntervalCPres, aes(interval, distance)) +
+  geom_point(aes(color=treatment)) + stat_smooth(method = "loess", se = F, size = 2, aes(color=treatment)) +
+  xlab('Time Interval') +
+  ylab('Euclidean Distance') +
+  scale_linetype(guide='none') +
+  scale_color_manual(values=c('#3300FF', '#FF3333'), name='Treatment', labels=c('high NPK+', 'control')) +
+  annotate('text', x=0, y=5, label='(d) Presence-based', size=10, hjust='left') +
+  theme(legend.position='none')
+
+pushViewport(viewport(layout=grid.layout(2,2)))
+print(rateChangeIntervalCAbundFig, vp=viewport(layout.pos.row=1, layout.pos.col=1))
+print(rateChangeIntervalCPresFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
+print(rateChangeIntervalCAbundFigB, vp=viewport(layout.pos.row=2, layout.pos.col=1))
+print(rateChangeIntervalCPresFigB, vp=viewport(layout.pos.row=2, layout.pos.col=2))
 #export as 1500x1500
 
 
