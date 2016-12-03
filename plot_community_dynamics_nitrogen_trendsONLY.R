c1d123cb464d5d993a35c9517184c813ed39b5ef
Andrew Tredennick <atredenn@gmail.com>
2016-12-02 17:03:11 -0800

    updates plotting

--- plot_community_dynamics_nitrogen_trendsONLY.R
+++ plot_community_dynamics_nitrogen_trendsONLY.R
@@ -53,7 +53,7 @@ colnames(bray_curtis_obs_only)[which(colnames(bray_curtis_obs_only)=="treat_year
 bray_curtis_obs_only$metric <- "bray_curtis"
 
 comm_metrics <- read.csv("~/Desktop/community_metrics.csv")
-torms <- which(comm_metrics$metric %in% c("appearance", "disappearance","bp_dominance","richness","turnover"))
+# torms <- which(comm_metrics$metric %in% c("appearance", "disappearance","bp_dominance","richness","turnover"))
 comm_metrics <- comm_metrics[-torms,]
 comm_metrics$site_proj_comm <- with(comm_metrics, paste0(site_code, project_name, community_type))
 
@@ -69,11 +69,11 @@ comm_metrics <- comm_metrics[,c("site_proj_comm", "treatment_year","metric","lrr
 comm_metrics <- rbind(comm_metrics, bray_curtis_obs_only[,c("site_proj_comm", "treatment_year","metric","lrr")])
 comm_metrics <- rbind(comm_metrics, anpp_lrr[,c("site_proj_comm", "treatment_year","metric","lrr")])
 comm_metrics <- rbind(comm_metrics, ttt[,c("site_proj_comm", "treatment_year","metric","lrr")])
-
-comm_metrics$metric <- factor(comm_metrics$metric, 
-                              levels=c("ANPP","bray_curtis","invD/S", "RankCor", "loss", "gain", "ppt"), 
-                              labels = c("ANPP","Bray-Curtis","Evenness", "Rank Corr.", "Loss", "Gain", "Precip."))
-
+# 
+# comm_metrics$metric <- factor(comm_metrics$metric, 
+#                               levels=c("ANPP","bray_curtis","invD/S", "RankCor", "loss", "gain", "ppt"), 
+#                               labels = c("ANPP","Bray-Curtis","Evenness", "Rank Corr.", "Loss", "Gain", "Precip."))
+# 
 
 sites_to_plot <- intersect(unique(bray_curtis_obs_only$site_proj_comm), unique(anpp_lrr$site_proj_comm))
 comm_metrics <- comm_metrics[which(comm_metrics$site_proj_comm %in% sites_to_plot),]
@@ -113,55 +113,9 @@ for(do_unit in unique(comm_metrics$site_proj_comm)){
 }
 
 
-mycols <- viridis(5, end=0.7)
-plot_list <- list()
-count <- 1
-for(do_site in unique(gam_df$site_proj_comm)){
-  temp_dat <- subset(gam_df, site_proj_comm==do_site)
-  minyr <- min(temp_dat$treatment_year)
-  maxyr <- max(temp_dat$treatment_year)
-  p <- ggplot(temp_dat, aes(x=treatment_year))+
-    geom_point(aes(y=lrr, group=site_proj_comm), alpha=0.5, color=mycols[count])+
-    geom_line(aes(y=yhat), color=mycols[count])+
-    xlab("Treatment year")+
-    ylab("")+
-    scale_x_continuous(breaks = seq(minyr, maxyr,2))+
-    facet_wrap("metric", scales = "free", nrow=1)+
-    my_theme
-  plot_list[[do_site]] <- p
-  count <- count+1
-}
 
-
-g <- arrangeGrob(plot_list[[1]], plot_list[[2]],plot_list[[3]],
-                 plot_list[[4]],plot_list[[5]], ncol=1) #generates g
-ggsave(file="~/Desktop/all_metrics_timeseries2.png", g, width = 10, height=8, units="in", dpi=90) #saves g
-
-
-
-
-
-####
-####  SOMETHING DIFFERENT
-####
-
-gam_sub <- subset(gam_df, site_proj_comm=="NWTbowmanDryBowman" & metric%in%c("Evenness","Rank Corr."))
+gam_sub <- subset(gam_df, site_proj_comm=="KNZpplots0" & metric%in%c("invD/S","RankCor","bp_dominance","appearance","disappearance", "bray_curtis"))
 ggplot(data = gam_sub, aes(x=treatment_year))+
-  geom_line(aes(y=yhat, linetype=metric))
-
-
-
-
-####
-####  PLOT BIVARIATE RELATIONSHIPS
-####
-serc_data <- subset(comm_metrics, site_proj_comm=="SERCCXN0"  & metric!="Precip.")
-serc_wide <- dcast(serc_data, treatment_year~metric, value.var = "lrr")
-plot(serc_wide$Evenness, serc_wide$`Bray-Curtis`)
-abline(lm(`Bray-Curtis`~Evenness, data=serc_wide))
-summary(lm(`Bray-Curtis`~Evenness+Gain+Loss+`Rank Corr.`, data=serc_wide))
-summary(lm(ANPP~Evenness+Gain+Loss+`Rank Corr.`, data=serc_wide))
-summary(lm(ANPP~`Bray-Curtis`, data=serc_wide))
-
+  geom_line(aes(y=yhat, color=metric), size=1)
 
 
