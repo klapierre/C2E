08531653849d150dd155f35ffab718e3b7ab75fa
Meghan Avolio <meghanavolio@Meghans-MB.local>
2017-10-02 10:03:33 -0400

    making reordering work for corre

--- RAC Changes.R
+++ RAC Changes.R
@@ -86,6 +86,7 @@ gain_loss<-rbind(gain_loss, gl)
 
 ##add ranks 
 ranks<-corredat%>%
+  filter(relcov!=0)%>%
   tbl_df()%>%
   group_by(site_project_comm, calendar_year, plot_id)%>%
   mutate(rank=rank(-relcov, ties.method = "average"))%>%
@@ -99,61 +100,58 @@ for (i in 1:length(explist)){
     filter(site_project_comm==explist[i])
 
   wide<-subset%>%
-    spread(key=genus_species, value=relcov, fill=0) %>%
-    gather(key=genus_species, value=relcov)
-
-
-##add ranks dropping zeros
-codyndat_rank_pres<-codyndat_clean%>%
-  filter(abundance!=0)%>%
-  tbl_df()%>%
-  group_by(site_project_comm, experiment_year, plot_id)%>%
-  mutate(rank=rank(-abundance, ties.method = "average"))%>%
-  tbl_df()
+    spread(key=genus_species, value=relcov, fill=0)
+  
+  long<-wide%>%
+    gather(key=genus_species, value=relcov, 10:ncol(wide))
+  
+  addzero<-rbind(addzero, long)
+}
 
 ###make zero abundant species have the rank S+1 (the size of the species pool plus 1)
 ##pull out zeros
-codyndat_zeros<-codyndat_clean%>%
-  filter(abundance==0)
+corre_zeros<-addzero%>%
+  filter(relcov==0)
 ##get species richness for each year
-codyndat_S<-group_by(codyndat_clean, site_project_comm, experiment_year, plot_id)%>%
-  summarize(S=S(abundance))
+corre_S<-group_by(corredat, site_project_comm, calendar_year, plot_id)%>%
+  summarize(S=S(relcov))
 ##merge together make zero abundances rank S+1
-codyndat_zero_rank<-merge(codyndat_zeros, codyndat_S, by=c("site_project_comm","experiment_year","plot_id"))%>%
+corre_zero_rank<-merge(corre_zeros, corre_S, by=c("site_project_comm","calendar_year","plot_id"))%>%
   mutate(rank=S+1)%>%
   select(-S)%>%
   tbl_df()
 ##combine all
-codyndat_rank<-rbind(codyndat_rank_pres, codyndat_zero_rank)
+corre_rank<-rbind(ranks, corre_zero_rank)%>%
+  mutate(exp_plot=paste(site_project_comm, plot_id, sep="::"))
 
-##calculate reordering between time steps 3 ways, rank correlations, mean rank shifts not corrected, and mean ranks shifts corrected for the size of the speceis pool
+##calculate reordering between time steps by mean ranks shifts corrected for the size of the speceis pool
 
-reordering=data.frame(id=c(), experiment_year=c(), MRSc=c())#expeiment year is year of timestep2
+reordering=data.frame(id=c(), calendar_year=c(), MRSc=c())#expeiment year is year of timestep2
 
-spc_id<-unique(codyndat_rank$id)
+exp_plot_list<-unique(corre_rank$exp_plot)
 
-for (i in 1:length(spc_id)){
-  subset<-codyndat_rank%>%
-    filter(id==spc_id[i])
-  id<-spc_id[i]
+for (i in 1:length(exp_plot_list)){
+  subset<-corre_rank%>%
+    filter(exp_plot==exp_plot_list[i])
+  id<-exp_plot_list[i]
   
   splist<-subset%>%
-    select(species)%>%
+    select(genus_species)%>%
     unique()
-  sppool<-length(splist$species)
+  sppool<-length(splist$genus_species)
   
   #now get all timestep within an experiment
-  timestep<-sort(unique(subset$experiment_year))    
+  timestep<-sort(unique(subset$calendar_year))    
   
   for(i in 1:(length(timestep)-1)) {#minus 1 will keep me in year bounds NOT WORKING
     subset_t1<-subset%>%
-      filter(experiment_year==timestep[i])
+      filter(calendar_year==timestep[i])
     
     subset_t2<-subset%>%
-      filter(experiment_year==timestep[i+1])
+      filter(calendar_year==timestep[i+1])
     
-    subset_t12<-merge(subset_t1, subset_t2, by=c("species","id"), all=T)%>%
-      filter(abundance.x!=0|abundance.y!=0)
+    subset_t12<-merge(subset_t1, subset_t2, by=c("genus_species","site_project_comm","site_code","project_name","community_type"), all=T)%>%
+      filter(relcov.x!=0|relcov.y!=0)
     
     MRSc<-mean(abs(subset_t12$rank.x-subset_t12$rank.y))/nrow(subset_t12)
     
@@ -164,81 +162,8 @@ for (i in 1:length(spc_id)){
   }
 }
 
-codyndat_reorder<-reordering%>%
-  separate(id, c("site_project_comm","plot_id"), sep="::")%>%
-  group_by(site_project_comm, experiment_year)%>%
-  summarise(MRSc=mean(MRSc))
-
-##SIM dataset
-##add in zeros
-
-##add ranks 
-sim_rank_pres<-sim%>%
-  filter(abundance!=0)%>%
-  tbl_df()%>%
-  group_by(id, time, site)%>%
-  mutate(rank=rank(-abundance, ties.method = "average"))%>%
-  tbl_df()
-
-#adding zeros
-sim_addzero <- sim %>%
-  group_by(id) %>%
-  nest() %>%
-  mutate(spread_df = purrr::map(data, ~spread(., key=species, value=abundance, fill=0) %>%
-                                  gather(key=species, value=abundance, -site, -time, -id2))) %>%
-  unnest(spread_df)
-
-###make zero abundant species have the rank S+1 (the size of the species pool plus 1)
-##pull out zeros
-sim_zeros<-sim_addzero%>%
-  filter(abundance==0)
-##get species richness for each year
-sim_S<-group_by(sim, id, time, site, id2)%>%
-  summarize(S=S(abundance))
-##merge together make zero abundances rank S+1
-sim_zero_rank<-merge(sim_zeros, sim_S, by=c("id","time","site", "id2"))%>%
-  mutate(rank=S+1)%>%
-  select(-S)%>%
-  tbl_df()
-##combine all
-sim_rank<-rbind(sim_rank_pres, sim_zero_rank)
-
-##calculating re-ordering
-
-reordering=data.frame(id=c(), time=c(), MRSc=c())#expeiment year is year of timestep2
-
-spc_id<-unique(sim_rank$id2)
-
-for (i in 1:length(spc_id)){
-  subset<-sim_rank%>%
-    filter(id2==spc_id[i])
-  id2<-spc_id[i]
-  #now get all timestep within an experiment
-  timestep<-sort(unique(subset$time))    
-  
-  for(i in 1:(length(timestep)-1)) {#minus 1 will keep me in year bounds NOT WORKING
-    subset_t1<-subset%>%
-      filter(time==timestep[i])
-    
-    subset_t2<-subset%>%
-      filter(time==timestep[i+1])
-    
-    subset_t12<-merge(subset_t1, subset_t2, by=c("species","id2"), all=T)%>%
-      filter(abundance.x!=0|abundance.y!=0)
-    
-    MRSc<-mean(abs(subset_t12$rank.x-subset_t12$rank.y))/nrow(subset_t12)
-    
-    metrics<-data.frame(id2=id2, time=timestep[i+1], MRSc=MRSc)#spc_id
-    ##calculate differences for these year comparison and rbind to what I want.
-    
-    reordering=rbind(metrics, reordering)  
-  }
-}
-
-sim_reorder<-reordering%>%
-  separate(id2, c("id","site"), sep="::")%>%
-  group_by(id, time)%>%
-  summarise(MRSc=mean(MRSc))
+corre_reordering<-reordering%>%
+  separate(id, c("site_project_comm","plot_id"), sep="::")
 
 #####Calculating Bray-Curtis both comparing the mean community change between consequtive time steps and the change in dispersion between two time steps.
 ##Doing this for all years of an experiment at one time point because want to ensure all points are in the same space.
@@ -247,42 +172,36 @@ sim_reorder<-reordering%>%
 ###third: mean_change is the distance the centroids of consequtive years
 ####fourth: dispersion_diff is the average dispersion of plots within a treatment to treatment centriod then compared between consequtive years
 
-#Codyn dataset first
-
-#make a new dataframe with just the label;
-site_project_comm_u<-unique(codyndat_clean$site_project_comm)
-
 #makes an empty dataframe
-bray_curtis=data.frame(site_project_comm=c(), experiment_year=c(), bc_mean_change=c(), bc_dispersion_diff=c()) 
-
+bray_curtis=data.frame(site_project_comm=c(), calendar_year=c(), bc_mean_change=c(), bc_dispersion_diff=c()) 
 ##calculating bray-curtis mean change and disperison differecnes
-for(i in 1:length(site_project_comm_u)) {
+for(i in 1:length(explist)) {
   
   #subsets out each dataset
-  subset=codyndat_clean%>%
-    filter(site_project_comm==site_project_comm_u[i])%>%
-    select(site_project_comm, experiment_year, species, abundance, plot_id)
+  subset=corredat%>%
+    filter(site_project_comm==explist[i])%>%
+    select(site_project_comm, calendar_year, genus_species, relcov, plot_id)
   
   #get years
-  experiment_years<-sort(unique(subset$experiment_year))
+  experiment_years<-sort(unique(subset$calendar_year))
   
   #transpose data
   species=subset%>%
-    spread(species, abundance, fill=0)
+    spread(genus_species, relcov, fill=0)
   
   #calculate bray-curtis dissimilarities
   bc=vegdist(species[,4:ncol(species)], method="bray")
   
   #calculate distances of each plot to year centroid (i.e., dispersion)
-  disp=betadisper(bc, species$experiment_year, type="centroid")
+  disp=betadisper(bc, species$calendar_year, type="centroid")
   
   #getting distances between centroids over years; these centroids are in BC space, so that's why this uses euclidean distances
   cent_dist=as.matrix(vegdist(disp$centroids, method="euclidean"))
   
   ##extracting only the comparisions we want year x to year x=1.
   ###(experiment_year is year x+1
-  cent_dist_yrs=data.frame(site_project_comm=site_project_comm_u[i],
-                           experiment_year=experiment_years[2:length(experiment_years)],
+  cent_dist_yrs=data.frame(site_project_comm=explist[i],
+                           calendar_year=experiment_years[2:length(experiment_years)],
                            mean_change=diag(cent_dist[2:nrow(cent_dist),1:(ncol(cent_dist)-1)]))
   
   #collecting and labeling distances to centroid from betadisper to get a measure of dispersion and then take the mean for a year
@@ -308,300 +227,11 @@ for(i in 1:length(site_project_comm_u)) {
 
 codyndat_braycurtis<-bray_curtis
 
-#Sim dataset
-
-#make a new dataframe with just the label;
-id_u<-unique(sim$id)
-
-#makes an empty dataframe
-bray_curtis=data.frame(id=c(), time=c(), bc_mean_change=c(), bc_dispersion_diff=c()) 
-
-#Calculating bc mean change and dispersion
-for(i in 1:length(id_u)) {
-  
-  #subsets out each dataset
-  subset=sim%>%
-    filter(id==id_u[i])%>%
-    select(id, time, species, abundance, site)
-  
-  #get years
-  timestep<-sort(unique(subset$time))
-  
-  #transpose data
-  species=subset%>%
-    spread(species, abundance, fill=0)
-  
-  #calculate bray-curtis dissimilarities
-  bc=vegdist(species[,4:ncol(species)], method="bray")
-  
-  #calculate distances of each plot to year centroid (i.e., dispersion)
-  disp=betadisper(bc, species$time, type="centroid")
-  
-  #getting distances between centroids over years; these centroids are in BC space, so that's why this uses euclidean distances
-  cent_dist=as.matrix(vegdist(disp$centroids, method="euclidean"))
-  
-  ##extracting only the comparisions we want year x to year x=1.
-  ###(experiment_year is year x+1
-  cent_dist_yrs=data.frame(id=id_u[i],
-                           time=timestep[2:length(timestep)],
-                           mean_change=diag(cent_dist[2:nrow(cent_dist),1:(ncol(cent_dist)-1)]))
-  
-  #collecting and labeling distances to centroid from betadisper to get a measure of dispersion and then take the mean for a year
-  disp2=data.frame(id=id_u[i],
-                   time=species$time,
-                   site=species$site,
-                   dist=disp$distances)%>%
-    tbl_df%>%
-    group_by(id, time)%>%
-    summarize(dispersion=mean(dist))
-  
-  ##subtract consequtive years subtracts year x+1 - x. So if it is positive there was greater dispersion in year x+1 and if negative less dispersion in year x+1
-  disp_yrs=data.frame(id=id_u[i],
-                      time=timestep[2:length(timestep)],
-                      dispersion_diff=diff(disp2$dispersion))
-  
-  #merge together change in mean and dispersion data
-  distances<-merge(cent_dist_yrs, disp_yrs, by=c("id","time"))
-  
-  #pasting dispersions into the dataframe made for this analysis
-  bray_curtis=rbind(distances, bray_curtis)  
-}
-sim_bray_curtis<-bray_curtis
-
-####Looking at the shape of the curve - cc
-###this compares the areas of difference between two curves that are consequtive time steps for a plot.
-
-#codyn dat first
-d_output=data.frame(site_project_comm=c(), experiment_year=c(), plot_id=c(), Darea=c())#expeiment year is year of timestep2
-
-spc<-unique(codyndat_clean$site_project_comm)
-
-for (i in 1:length(spc)){
-  subset<-codyndat_clean%>%
-    filter(site_project_comm==spc[i])
-  
-  ranks<-subset%>%
-    filter(abundance!=0)%>%
-    group_by(experiment_year, plot_id)%>%
-    mutate(rank=rank(-abundance, ties.method="average"),
-           maxrank=max(rank),
-           relrank=rank/maxrank)%>%
-    arrange(-abundance)%>%
-    mutate(cumabund=cumsum(abundance))%>%
-    ungroup()
-  
-  spc_id2<-spc[i]
-  
-  timestep<-sort(unique(ranks$experiment_year))    
-  
-  for(i in 1:(length(timestep)-1)) {#minus 1 will keep me in year bounds NOT WORKING
-    subset_t1<-ranks%>%
-      filter(experiment_year==timestep[i])
-    
-    plots_t1<-subset_t1%>%
-      select(plot_id)%>%
-      unique()
-    
-    subset_t2<-ranks%>%
-      filter(experiment_year==timestep[i+1])
-    
-    plots_t2<-subset_t2%>%
-      select(plot_id)%>%
-      unique()
-    
-    plots_bothyrs<-merge(plots_t1, plots_t2, by="plot_id")
-    #dataset of two years    
-    subset_t12<-rbind(subset_t1, subset_t2)
-    
-    ##dropping plots that were not measured both years
-    subset_t12_2<-merge(plots_bothyrs, subset_t12, by="plot_id")
-    
-    #dropping plots with only 1 species in any of the two years    
-    drop<-subset_t12_2%>%
-      group_by(experiment_year, plot_id)%>%
-      mutate(numplots=length(plot_id))%>%
-      ungroup()%>%
-      group_by(plot_id)%>%
-      mutate(min=min(numplots))%>%
-      select(plot_id, min)%>%
-      unique()
-    
-    subset_t12_3<-merge(subset_t12_2, drop, by="plot_id")%>%
-      filter(min!=1)%>%
-      ungroup()%>%
-      group_by(experiment_year, plot_id)%>%
-      arrange(rank)%>%
-      ungroup()
-    
-    result <- subset_t12_3 %>%
-      group_by(plot_id) %>%
-      do({
-        y <- unique(.$experiment_year)###assumption this is a length 2 list
-        df1 <- filter(., experiment_year==y[[1]])
-        df2 <- filter(., experiment_year==y[[2]])
-        sf1 <- stepfun(df1$relrank, c(0, df1$cumabund))
-        sf2 <- stepfun(df2$relrank, c(0, df2$cumabund))
-        r <- sort(unique(c(0, df1$relrank, df2$relrank)))
-        h <- abs(sf1(r) - sf2(r))
-        w <- c(diff(r), 0)
-        data.frame(Dstar=sum(w*h))#do has to output a dataframe
-      })
-    
-    d_output1=data.frame(site_project_comm=spc_id2, experiment_year=timestep[i+1], plot_id=result$plot_id, Dstar=result$Dstar)#expeiment year is year of timestep2
-    
-    d_output<-rbind(d_output, d_output1)
-  }
-}
-codyndat_dstar<-d_output%>% 
-  group_by(site_project_comm, experiment_year)%>%
-  summarise(Dstar=mean(Dstar))
-
-####Looking at the shape of the curve - cc
-#sim dataset
-d_output=data.frame(id=c(), time=c(), site=c(), Darea=c())#expeiment year is year of timestep2
-
-com<-unique(sim$id)
-
-for (i in 1:length(com)){
-  subset<-sim%>%
-    filter(id==com[i])
-  
-  ranks<-subset%>%
-    filter(abundance!=0)%>%
-    group_by(time, site)%>%
-    mutate(rank=rank(-abundance, ties.method="average"),
-           maxrank=max(rank),
-           relrank=rank/maxrank)%>%
-    arrange(-abundance)%>%
-    mutate(cumabund=cumsum(abundance))%>%
-    ungroup()
-  
-  com_id2<-com[i]
-  
-  timestep<-sort(unique(ranks$time))    
-  
-  for(i in 1:(length(timestep)-1)) {#minus 1 will keep me in year bounds NOT WORKING
-    subset_t1<-ranks%>%
-      filter(time==timestep[i])
-    
-    plots_t1<-subset_t1%>%
-      select(site)%>%
-      unique()
-    
-    subset_t2<-ranks%>%
-      filter(time==timestep[i+1])
-    
-    plots_t2<-subset_t2%>%
-      select(site)%>%
-      unique()
-    
-    plots_bothyrs<-merge(plots_t1, plots_t2, by="site")
-    #dataset of two years    
-    subset_t12<-rbind(subset_t1, subset_t2)
-    
-    ##dropping plots that were not measured both years
-    subset_t12_2<-merge(plots_bothyrs, subset_t12, by="site")
-    
-    #dropping plots with only 1 species in any of the two years    
-    drop<-subset_t12_2%>%
-      group_by(time, site)%>%
-      mutate(numplots=length(site))%>%
-      ungroup()%>%
-      group_by(site)%>%
-      mutate(min=min(numplots))%>%
-      select(site, min)%>%
-      unique()
-    
-    subset_t12_3<-merge(subset_t12_2, drop, by="site")%>%
-      filter(min!=1)%>%
-      ungroup()%>%
-      group_by(time, site)%>%
-      arrange(rank)%>%
-      ungroup()
-    
-    result <- subset_t12_3 %>%
-      group_by(site) %>%
-      do({
-        y <- unique(.$time)###assumption this is a length 2 list
-        df1 <- filter(., time==y[[1]])
-        df2 <- filter(., time==y[[2]])
-        sf1 <- stepfun(df1$relrank, c(0, df1$cumabund))
-        sf2 <- stepfun(df2$relrank, c(0, df2$cumabund))
-        r <- sort(unique(c(0, df1$relrank, df2$relrank)))
-        h <- abs(sf1(r) - sf2(r))
-        w <- c(diff(r), 0)
-        data.frame(Dstar=sum(w*h))#do has to output a dataframe
-      })
-    
-    d_output1=data.frame(id=com_id2, time=timestep[i+1], site=result$site, Dstar=result$Dstar)#expeiment year is year of timestep2
-    
-    d_output<-rbind(d_output, d_output1)
-  }
-}
-
-sim_dstar<-d_output%>% 
-  group_by(id, time)%>%
-  summarise(Dstar=mean(Dstar))
-
-#######trying to look at spatial differences.
-sim_subset<-sim%>%
-  separate(id, into=c("alpha", "even", "comtype", "rep"), sep="_")%>%
-  filter(time==1&rep==1)%>%
-  mutate(alphaeven=paste(alpha, even, sep="_"))
-
-sp_output=data.frame()
-
-id<-unique(sim_subset$alphaeven)
-
-for (i in 1:length(id)){
-  species<-sim_subset%>%
-    filter(alphaeven==id[i])%>%
-    spread(species, abundance, fill=0)
-  
-  mds<-metaMDS(species[,9:ncol(species)])
-  
-  info<-species[,1:8]
-  
-  scores <- data.frame(scores(mds, display="sites"))
-  scores2<- cbind(info, scores)
-  
-  sp_output<-rbind(sp_output,scores2)
-}
-
-theme_set(theme_bw(12))
-ggplot(data=sp_output, aes(x=NMDS1, y=NMDS2, color=comtype))+
-  geom_point()+
-  scale_color_manual(values=c("black","red","green","blue"))+
-  facet_wrap(~alphaeven, ncol=3, scales="free")
 
 ####MERGING TO A SINGE DATASET
-#codyn
 merge1<-merge(codyndat_diversity, codyndat_gains_loss, by=c("site_project_comm","experiment_year"))
 merge2<-merge(merge1, codyndat_reorder, by=c("site_project_comm","experiment_year"))
 merge3<-merge(merge2, codyndat_braycurtis, by=c("site_project_comm","experiment_year"))
 merge4<-merge(merge3, codyndat_dstar, by=c("site_project_comm","experiment_year"))
 codyndat_allmetrics<-merge(merge4, codyndat_info, by="site_project_comm")
-
-#sim
-merge1<-merge(sim_diversity, sim_gains_loss, by=c("id","time"))
-merge2<-merge(merge1, sim_reorder, by=c("id","time"))
-merge3<-merge(merge2, sim_bray_curtis, by=c("id","time"))
-sim_allmetrics<-merge(merge3, sim_dstar, by=c("id","time"))%>%
-  separate(id, into=c("alpha","even","comtype","rep"), sep="_")%>%
-  group_by(alpha, even, comtype, time)%>%
-  summarize(S=mean(S), E_Q=mean(E_Q), gain=mean(gain), loss=mean(loss), MRSc=mean(MRSc), mean_change=mean(mean_change), dispersion_diff=mean(dispersion_diff), Dstar=mean(Dstar))
-
-sim_allmetrics$comtype2<-as.factor(sim_allmetrics$comtype)
-
-#graphing this
-panel.pearson <- function(x, y, ...) {
-  horizontal <- (par("usr")[1] + par("usr")[2]) / 2; 
-  vertical <- (par("usr")[3] + par("usr")[4]) / 2; 
-  text(horizontal, vertical, format(abs(cor(x,y)), digits=2)) 
-}
-
-pairs(sim_allmetrics[,c(5:12)], col=sim_allmetrics$comtype2, upper.panel = panel.pearson)
-pairs(codyndat_allmetrics[,c(3:4,7:12)], col=codyndat_allmetrics$num_plots, upper.panel = panel.pearson)
-
-
 
