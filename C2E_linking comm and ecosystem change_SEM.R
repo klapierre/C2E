4ec8f5e8b391fb10eda6317e617d8c6a05931179
La Pierre <LaPierreK@SI.EDU>
2017-10-04 16:49:45 -0400

    ANPP change with new treatments.

---
+++ C2E_linking comm and ecosystem change_SEM.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(ggplot2)
+library(grid)
+
+#kim's desktop
+setwd('C:\\Users\\la pierrek\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm')
+
+correCommChange <- read.csv('CORRE_ContTreat_Compare_OCT2017.csv')
+
+correANPPchange <- read.csv('')
