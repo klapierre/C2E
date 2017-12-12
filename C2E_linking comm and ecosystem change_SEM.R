b2acc963845869cd659d7f45061ec7db12b7746d
La Pierre <LaPierreK@SI.EDU>
2017-12-12 15:33:29 -0500

    Dealing with outliers in ANPP data.

--- C2E_linking comm and ecosystem change_SEM.R
+++ C2E_linking comm and ecosystem change_SEM.R
@@ -24,10 +24,10 @@ theme_update(axis.title.x=element_text(size=40, vjust=-0.35, margin=margin(t=15)
 #######MODEL STRUCTURE: use differences between treatment and control plots in each year (horizontal arrows); include treatments as factors at the bottom, with multi-factor treatments having more than one greater than 0 (and make them categorical response variables)-------------------
 ####data input
 ###add treatment info to get binary treatments of various manipulation types
-trt <- read.csv('ExperimentInformation_Nov2017.csv')%>%
+trt <- read.csv('ExperimentInformation_anpp_Dec2017.csv')%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep='_'))%>%
-  select(-site_code, -project_name, -community_type, -treatment_year, -public)
+  select(-site_code, -project_name, -community_type, -public)
 
 ###community data
 correCommChange <- read.csv('CORRE_ContTreat_Compare_Nov2017.csv')%>%
@@ -39,12 +39,24 @@ correANPP <- read.csv('ANPP_Dec2017.csv')%>%
   select(-X)%>%
   mutate(treatment_year_2=ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2004, 10, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2005, 11, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2006, 12, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2007, 13, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2008, 14, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2009, 15, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2010, 16, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2011, 17, ifelse(site_project_comm=='SEV_Nfert_0'&calendar_year==2012, 18, treatment_year))))))))))%>%
   select(-treatment_year)%>%mutate(treatment_year=treatment_year_2)%>%select(-treatment_year_2)%>%
-  #remove subset of CDR trts
-  mutate(drop=ifelse(site_code=='CDR'&treatment=='2', 1, ifelse(site_code=='CDR'&treatment=='3', 1, ifelse(site_code=='CDR'&treatment=='4', 1, ifelse(site_code=='CDR'&treatment=='5', 1, ifelse(site_code=='CDR'&treatment=='7', 1, ifelse(site_code=='CDR'&treatment=='2_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='3_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='4_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='5_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='7_f_u_n', 1, 0)))))))))))%>%
+  #remove subset of CDR trts and KNZ BGP mowed treatment (they herbicided)
+  mutate(drop=ifelse(site_code=='CDR'&treatment=='2', 1, ifelse(site_code=='CDR'&treatment=='3', 1, ifelse(site_code=='CDR'&treatment=='4', 1, ifelse(site_code=='CDR'&treatment=='5', 1, ifelse(site_code=='CDR'&treatment=='7', 1, ifelse(site_code=='CDR'&treatment=='2_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='3_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='4_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='5_f_u_n', 1, ifelse(site_code=='CDR'&treatment=='7_f_u_n', 1, ifelse(project_name=='BGP'&treatment=='u_m_n', 1, ifelse(project_name=='BGP'&treatment=='u_m_p', 1, ifelse(project_name=='BGP'&treatment=='u_m_b', 1, ifelse(project_name=='BGP'&treatment=='u_m_c', 1, ifelse(project_name=='BGP'&treatment=='b_m_n', 1, ifelse(project_name=='BGP'&treatment=='b_m_p', 1, ifelse(project_name=='BGP'&treatment=='b_m_b', 1, ifelse(project_name=='BGP'&treatment=='b_m_c', 1, 0)))))))))))))))))))%>%
   #remove NANT wet because it only has ANPP in one year and has a much higher rate of N added (67.2 gm-2)
   filter(site_code!='NANT')%>%
   filter(drop==0)%>%
+  ####NOTE: check outliers with data providers and omit this step when data is correct
+  mutate(outlier=ifelse(site_code=='ORNL'&anpp>1080, 1, ifelse(project_name=='snow'&anpp>1010, 1, ifelse(project_name=='T7'&anpp>1860, 1, 0))))%>%
+  filter(outlier==0)%>%
+  filter(anpp>0)%>%
+  select(-drop, -outlier)%>%
   left_join(trt)
+
+
+# #checking site level data for outliers in anpp
+# ggplot(correANPP, aes(anpp)) + geom_histogram() + facet_wrap(~project_name, scales='free')
+
+
+#calculating anpp difference
 #anpp ctl data
 correANPPctl <- correANPP%>%
   filter(plot_mani==0)%>%
@@ -82,6 +94,7 @@ chart.Correlation(dataVis, histogram=T, pch=19)
 # siteInfo <- read.csv('SiteExperimentDetails_Dec2016.csv')%>%
 #   left_join(correSEMdata)
 # with(siteInfo, plot(Ed~rrich)) #yes! but we can transform.
+
 
 
 #keep all data
@@ -645,7 +658,7 @@ ggplot(data=subset(stdEst, lhs=='anpp_PC_transform'&rhs!=''&rhs!='anpp_PC_transf
   geom_hline(yintercept=0) +
   facet_wrap(~rhs)
 
-ggplot(data=subset(stdEst, lhs=='mean_change_transform'&rhs!=''&rhs!='anpp_PC_transform'&rhs!='mean_change_transform'), aes(x=treatment_year, y=std_alt)) +
+ggplot(data=subset(stdEst, lhs=='mean_change_transform'&rhs!=''&rhs!='anpp_PC_transform'&rhs!='mean_change_transform'), aes(x=treatment_year, y=std.all)) +
   geom_point(size=3) +
   geom_errorbar(aes(ymin=std.all-se, ymax=std.all+se)) +
   geom_line(size=2) +
@@ -653,7 +666,7 @@ ggplot(data=subset(stdEst, lhs=='mean_change_transform'&rhs!=''&rhs!='anpp_PC_tr
   geom_hline(yintercept=0) +
   facet_wrap(~rhs)
 
-ggplot(data=subset(stdEst, lhs!='anpp_PC_transform'&lhs!='mean_change_transform'&rhs!=''&rhs!='anpp_PC_transform'&rhs!='mean_change_transform'&rhs!='Rd'&rhs!='Ed_transform'&rhs!='Sd_transform'&rhs!='spd'), aes(x=treatment_year, y=std_alt, color=rhs)) +
+ggplot(data=subset(stdEst, lhs!='anpp_PC_transform'&lhs!='mean_change_transform'&rhs!=''&rhs!='anpp_PC_transform'&rhs!='mean_change_transform'&rhs!='Rd'&rhs!='Ed_transform'&rhs!='Sd_transform'&rhs!='spd'), aes(x=treatment_year, y=std.all, color=rhs)) +
   geom_point(size=3) +
   geom_line(size=2) +
   ylab('Effect Size') + xlab('Treatment Year') +
