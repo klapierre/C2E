8c190deb85e3595d9ba907fd78e28a03c37a961b
Meghan Avolio <meghanavolio@Meghans-MB.local>
2018-03-20 12:10:26 -0400

    adding info on files

--- Community Paper/permanova loop.R
+++ Community Paper/permanova loop.R
@@ -3,6 +3,42 @@
 ### Authors: Kevin Wilcox (wilcoxkr@gmail.com) Andrew Tredennick (atredenn@gmail.com)
 ### Last updated: March 20 2018
 
+corredat1<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+  select(-X)%>%
+  mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
+  filter(site_project_comm!="GVN_FACE_0", site_project_comm!="AZI_NitPhos_0", site_project_comm!="JRN_study278_0", site_project_comm!="KNZ_GFP_4F", site_project_comm!="Saskatchewan_CCD_0")
+
+##several studies only have two measurments of a plot. I am dropping those plots
+azi<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+  select(-X)%>%
+  mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
+  filter(site_code=="AZI")%>%
+  filter(plot_id!=11&plot_id!=15&plot_id!=35&plot_id!=37)
+
+jrn<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+  select(-X)%>%
+  mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
+  filter(site_project_comm=="JRN_study278_0")%>%
+  filter(plot_id!=211&plot_id!=210)
+
+knz<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+  select(-X)%>%
+  mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
+  filter(site_project_comm=="KNZ_GFP_4F")%>%
+  filter(plot_id!="7_1_1"&plot_id!="7_2_1")
+
+sak<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+  select(-X)%>%
+  mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
+  filter(site_project_comm=="Saskatchewan_CCD_0")%>%
+  filter(plot_id!=2)
+
+corredat<-rbind(corredat1, azi, jrn, knz, sak)
+
+trt<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/ExperimentInformation_Nov2017.csv")%>%
+  select(site_code, project_name, community_type, treatment,plot_mani)%>%
+  unique()%>%
+  mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))
 
 
 control<-ranks%>%
