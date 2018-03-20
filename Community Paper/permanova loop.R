378728b899cafb6fb526b55907fba298158fcf3d
wilcoxkr <wilcoxkr@gmail.com>
2018-03-20 14:38:45 -0400

    working -

--- Community Paper/permanova loop.R
+++ Community Paper/permanova loop.R
@@ -3,66 +3,116 @@
 ### Authors: Kevin Wilcox (wilcoxkr@gmail.com) Andrew Tredennick (atredenn@gmail.com)
 ### Last updated: March 20 2018
 
-corredat1<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+### Set up workspace
+setwd("C:\\Users\\wilco\\Dropbox\\")
+library(tidyverse)
+library(vegan)
+library(ggthemes)
+corredat1<-read.csv("C2E\\Products\\CommunityChange\\March2018 WG\\data\\SpeciesRelativeAbundance_Oct2017.csv")%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
   filter(site_project_comm!="GVN_FACE_0", site_project_comm!="AZI_NitPhos_0", site_project_comm!="JRN_study278_0", site_project_comm!="KNZ_GFP_4F", site_project_comm!="Saskatchewan_CCD_0")
 
 ##several studies only have two measurments of a plot. I am dropping those plots
-azi<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+azi<-read.csv("C2E\\Products\\CommunityChange\\March2018 WG\\data\\SpeciesRelativeAbundance_Oct2017.csv")%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
   filter(site_code=="AZI")%>%
   filter(plot_id!=11&plot_id!=15&plot_id!=35&plot_id!=37)
 
-jrn<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+jrn<-read.csv("C2E\\Products\\CommunityChange\\March2018 WG\\data\\SpeciesRelativeAbundance_Oct2017.csv")%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
   filter(site_project_comm=="JRN_study278_0")%>%
   filter(plot_id!=211&plot_id!=210)
 
-knz<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+knz<-read.csv("C2E\\Products\\CommunityChange\\March2018 WG\\data\\SpeciesRelativeAbundance_Oct2017.csv")%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
   filter(site_project_comm=="KNZ_GFP_4F")%>%
   filter(plot_id!="7_1_1"&plot_id!="7_2_1")
 
-sak<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
+sak<-read.csv("C2E\\Products\\CommunityChange\\March2018 WG\\data\\SpeciesRelativeAbundance_Oct2017.csv")%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
   filter(site_project_comm=="Saskatchewan_CCD_0")%>%
   filter(plot_id!=2)
 
-corredat<-rbind(corredat1, azi, jrn, knz, sak)
+corredat_raw <-rbind(corredat1, azi, jrn, knz, sak)
 
-trt<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/ExperimentInformation_Nov2017.csv")%>%
-  select(site_code, project_name, community_type, treatment,plot_mani)%>%
+treatment_info<-read.csv("C2E\\Products\\CommunityChange\\March2018 WG\\data\\ExperimentInformation_Nov2017.csv")%>%
+  dplyr::select(site_code, project_name, community_type, treatment,plot_mani)%>%
   unique()%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))
 
+corredat <- corredat_raw %>%
+  filter(site_project_comm != "GVN_FACE_0") %>%
+  left_join(treatment_info, by=c( "site_code","project_name","community_type", "treatment","site_project_comm"))
 
-control<-ranks%>%
-  filter(C_T=="Control")
+site_project_comm_vec <- unique(corredat$site_project_comm)
 
-treat_list<-unique(subset(ranks, C_T=="Treatment")$treatment)
+permanova_out_master <- {}
 
-for(i in 1:length(treat_list)) {
-  subset_trt<-ranks%>%
-    filter(treatment==treat_list[i])
+for(i in 1:length(site_project_comm_vec)) {
+  corredat_temp <- filter(corredat, site_project_comm==site_project_comm_vec[i])
+  control_temp <- filter(corredat_temp, plot_mani==0)
+  treatment_temp <- filter(corredat_temp, plot_mani !=0)
   
-  #dataset of two treatments    
-  subset_t12<-rbind(control, subset_trt)
+  treatment_vec <- unique(treatment_temp$treatment)
   
-  result <- subset_t12 %>%
-    group_by(time) %>%
-    do({
-      y <- unique(.$treatment)###assumption this is a length 2 list
-      df1 <- filter(., treatment==y[[1]])
-      df2 <- filter(., treatment==y[[2]])
-      sf1 <- stepfun(df1$relrank, c(0, df1$cumabund))
-      sf2 <- stepfun(df2$relrank, c(0, df2$cumabund))
-      r <- sort(unique(c(0, df1$relrank, df2$relrank)))
-      h <- abs(sf1(r) - sf2(r))
-      w <- c(diff(r), 0)
-      data.frame(CC=sum(w*h))#do has to output a dataframe
-    })
+  for(trt in 1:length(treatment_vec)){
+    treatment_temp_2 <- filter(treatment_temp, treatment==treatment_vec[trt])
+    control_years_vec <- unique(control_temp$calendar_year)
+    treatment_years_vec <- unique(treatment_temp_2$calendar_year)
+    
+    year_keeper <- intersect(control_years_vec, treatment_years_vec)
+    
+    one_treatment_temp <- filter(treatment_temp, treatment==treatment_vec[trt]) %>%
+      filter(calendar_year %in% year_keeper)
+    control_temp_2 <- filter(control_temp, calendar_year %in% year_keeper)
+
+    trt_ctrl_df_temp <- one_treatment_temp %>%
+      bind_rows(control_temp_2)
+
+    year_vec <- unique(trt_ctrl_df_temp$calendar_year)
+    
+    for(yr in 1:length(year_vec)){
+      
+      trt_ctrl_yr_temp <- trt_ctrl_df_temp %>%
+        filter(calendar_year==year_vec[yr]) %>%
+        spread(key=genus_species, value=relcov, fill=0)
+      
+      cover_temp <- trt_ctrl_yr_temp %>%
+        dplyr::select(-(site_code:plot_mani))
+
+      env_temp <- trt_ctrl_yr_temp %>%
+        dplyr::select(site_code:plot_mani)
+      
+      permanova_temp <- adonis(cover_temp ~ plot_mani, data=env_temp, permutations=99)
+
+      perm_out_temp <- data.frame(
+        site_project_comm = site_project_comm_vec[i],
+        treatment = treatment_vec[trt],
+        calendar_year = year_vec[yr],
+        Pvalue =  permanova_temp$aov.tab$'Pr(>F)'[1]
+      )
+      
+      permanova_out_master <- rbind(permanova_out_master, perm_out_temp)
+     
+    }
+  }
+}
+       
+permanova_out_mod <- permanova_out_master %>%
+  mutate(pval_flag = ifelse(Pvalue<.05, 1, 0)) %>%
+  mutate(permanova="permanova") %>%
+  group_by(site_project_comm, treatment) %>%
+  summarise(tot_pval=sum(pval_flag)) %>%
+  filter(tot_pval != 0)
+
+#write.csv(permanova_out_mod, file= "C2E\\Products\\CommunityChange\\March2018 WG\\permanova out.csv",row.names=F)
+
+
+
+
+
