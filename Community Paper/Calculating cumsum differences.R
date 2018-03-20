798c0ef3d259b11f213d50d932bacbccd397d611
wilcoxkr <wilcoxkr@gmail.com>
2018-03-20 10:54:42 -0400

    Calculate cumulative differences for all data sets

---
+++ Community Paper/Calculating cumsum differences.R
@@ -0,0 +1,21 @@
+?cumsum
+rm(list=ls())
+df <- data.frame(group=c(rep("a",6),rep("b",6)), a=1:6, b=1:12)
+
+cumsum(df$a)
+
+g_csum <- df %>%
+  group_by(group) %>%
+  mutate(cumsum=cumsum(b))
+
+library(tidyverse)
+library(ggplot2)
+
+rr_full <- read.csv("C:\\Users\\wilco\\Dropbox\\C2E\\Products\\CommunityChange\\March2018 WG\\CORRE_RAC_LogRR_March2018_trtyr.csv")
+
+site_project_comm_vec <- unique(rr_full$site_project_comm)
+
+rr_test <- filter(rr_full, site_project_comm==site_project_comm_vec[1])
+
+rr_cumsum <- rr_test %>%
+  
