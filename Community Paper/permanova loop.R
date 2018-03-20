2bc0bad070ff41a95b4f2ffbcf61cf1b2ac8fe12
wilcoxkr <wilcoxkr@gmail.com>
2018-03-20 12:07:19 -0400

    Running permanovas for each experiment in each year

---
+++ Community Paper/permanova loop.R
@@ -0,0 +1,7 @@
+### calculating permanova for each experiment and year
+###
+### Authors: Kevin Wilcox (wilcoxkr@gmail.com) Andrew Tredennick (atredenn@gmail.com)
+### Last updated: March 20 2018
+
+
+
