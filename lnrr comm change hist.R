86e7a2c70300cd8e160b5403daa3576675d515aa
grmane <egrman@emich.edu>
2018-03-21 11:46:57 -0400

    updated working directories and added code to subset by 2 columns at once

--- lnrr comm change hist.R
+++ lnrr comm change hist.R
@@ -2,7 +2,11 @@ library(tidyverse)
 library(ggplot2)
 library(grid)
 
+#kim's working directory
 setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\C2E\\Products\\CommunityChange\\March2018 WG')
+
+#emily's working directory
+setwd("/Users/egrman/Dropbox/C2E/Products/CommunityChange/March2018 WG")
 
 theme_set(theme_bw())
 theme_update(axis.title.x=element_text(size=20, vjust=-0.35), axis.text.x=element_text(size=16),
@@ -14,11 +18,11 @@ theme_update(axis.title.x=element_text(size=20, vjust=-0.35), axis.text.x=elemen
 lnRR <- read.csv('CORRE_RAC_LogRR_March2018_trtyr.csv')
 metrics <- read.csv('CORRE_RAC_Metrics_March2018_trtyr.csv')
 
-trt <- read.csv('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm\\ExperimentInformation_Nov2017.csv')%>%
+trt <- read.csv('ExperimentInformation_Nov2017.csv')%>%
   mutate(site_project_comm=as.factor(paste(site_code, project_name, community_type, sep='_')))%>%
   select(-X)
 
-rawAbund <- read.csv('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm\\SpeciesRawAbundance_Oct2017.csv')%>%
+rawAbund <- read.csv('SpeciesRawAbundance_Oct2017.csv')%>%
   select(-X, -abundance, -genus_species, -block, -calendar_year, -treatment_year)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep='_'))%>%
   unique()
@@ -121,6 +125,8 @@ print(metricsGFig, vp=viewport(layout.pos.row = 5, layout.pos.col = 1))
 
 pplots <- metricsTrt%>%
   filter(treatment=='N1P0'|treatment=='N2P0'|treatment=='N2P3')
+
+pplots=metricsTrt[metricsTrt$treatment %in% c("N1P0", "N2P0", "N2P3") & metricsTrt$site_project_comm=="KNZ_pplots_0",]
 
 #lines of change for pplots across all treatements
 metricsSFig <- ggplot(data=pplots, aes(x=treatment_year, y=abs(richness_change), color=treatment)) +
