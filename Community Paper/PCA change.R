10e7a86ffb01276f03f0b83bd919656cc98142c4
wilcoxkr <wilcoxkr@gmail.com>
2018-03-22 14:07:05 -0400

    got rid of rows with nas

--- Community Paper/PCA change.R
+++ Community Paper/PCA change.R
@@ -13,9 +13,16 @@ source("C:\\Users\\wilco\\Desktop\\Git projects\\C2E\\Community Paper\\calculate
 glass_means <- change_glass_d %>%
   group_by(site_project_comm, treatment.x, plot_mani) %>%
   summarise_at(vars(abs_richness_glass:losses_glass), funs(mean)) %>%
-  replace_at()
+  mutate(abs_richness_glass = replace(abs_richness_glass, abs_richness_glass == "Inf", NA),
+         abs_evenness_glass = replace(abs_evenness_glass, abs_evenness_glass == "Inf", NA),
+         rank_glass = replace(rank_glass, rank_glass == "Inf", NA),
+         gains_glass = replace(gains_glass, gains_glass == "Inf", NA),
+         losses_glass = replace(losses_glass, losses_glass == "Inf", NA)) %>%
+  ungroup() %>%
+  na.omit()
 
-replace_
-?replace_all
-?prcomp
-pca_glass <- prcomp(dplyr::select(glass_means, abs_richness_glass:losses_glass))
+filter(glass_means, is.na(abs_evenness_glass))
+
+pca_glass <- prcomp(dplyr::select(glass_means, abs_richness_glass:losses_glass), scale.=T)
+
+pca_glass$x
