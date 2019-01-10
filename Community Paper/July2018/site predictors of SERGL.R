f03a2335732d05f9ed4da756323175ea98cb3b6c
Meghan <meghan.avolio@gmail.com>
2019-01-10 16:04:43 -0500

    combining correlation and multiple regression figures.

--- Community Paper/July2018/site predictors of SERGL.R
+++ Community Paper/July2018/site predictors of SERGL.R
@@ -15,6 +15,7 @@ library(grid)
 library(vegan)
 library(rsq)
 library(car)
+library(gridExtra)
 
 
 theme_set(theme_bw())
@@ -355,6 +356,7 @@ levels(forbigfig$response)=c("Richness change", "Evenness change", "Rank change"
 levels(forbigfig$predictor)=c("(Intercept)", "ANPP", "MAP", "MAT", "Regional SR")
 forbigfig$significant=as.factor(1*(forbigfig$pval<0.05))
 forbigfig$star.location=ifelse(forbigfig$slope>0, forbigfig$slope+0.02, forbigfig$slope-0.02)
+mr<-
 ggplot(aes(predictor, slope, fill=rsq), data=forbigfig[!forbigfig$predictor=="(Intercept)",]) + 
   geom_col() + 
   geom_point(aes(predictor, star.location, shape=significant)) + 
@@ -365,14 +367,15 @@ ggplot(aes(predictor, slope, fill=rsq), data=forbigfig[!forbigfig$predictor=="(I
   guides(fill = guide_colorbar(title = "Partial R2")) + 
   scale_shape_manual(values=c(NA, 8), guide=FALSE)+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  geom_text(data=rsqvalues, aes(x=4.1, y = 0.25, label = combinded), hjust=1.05, vjust=1.5)
+  geom_text(data=rsqvalues, aes(x=4.1, y = 0.25, label = combinded), hjust=1.05, vjust=1.5)+
+  ggtitle("A)")
 
 
 #ggsave("Summer2018_Results/site predictors of SERGL/site predictors of SERGL.pdf", width=7, height=9.5)
 #ggsave("Summer2018_Results/site predictors of SERGL/site predictors of SERGL.png", width=7, height=9.5)
 
-ggplot(aes(response, slope, fill=rsq), data=forbigfig[!forbigfig$predictor=="(Intercept)" & !forbigfig$pval>0.05,]) + geom_col() + facet_grid(studies~predictor) + theme(axis.text.x=element_text(angle = 90, vjust = 0.4)) + xlab("") + ylab("Effect on aspect of community change\n(slope from standardized multiple regression)") + guides(fill = guide_colorbar(title = "Partial R2"))
-ggsave("Summer2018_Results/site predictors of SERGL/significant site predictors of SERGL.pdf", width=8, height=9.5)
+# ggplot(aes(response, slope, fill=rsq), data=forbigfig[!forbigfig$predictor=="(Intercept)" & !forbigfig$pval>0.05,]) + geom_col() + facet_grid(studies~predictor) + theme(axis.text.x=element_text(angle = 90, vjust = 0.4)) + xlab("") + ylab("Effect on aspect of community change\n(slope from standardized multiple regression)") + guides(fill = guide_colorbar(title = "Partial R2"))
+# ggsave("Summer2018_Results/site predictors of SERGL/significant site predictors of SERGL.pdf", width=8, height=9.5)
 
 
 
@@ -430,6 +433,7 @@ vari<-c(
   losses_glass = 'Species Losses'
 )
 
+cor<-
 ggplot(data=tograph_cor, aes(x = value, y = vari_value))+
   geom_point()+
   facet_grid(row = vars(vari_group), cols = vars(parm_group), scales="free", labeller=labeller(vari_group = vari, parm_group = parameter))+
@@ -441,143 +445,146 @@ ggplot(data=tograph_cor, aes(x = value, y = vari_value))+
   geom_smooth(data=subset(tograph_cor, vari_group=="gains_glass"&parm_group=="srrich"), method="lm", se=F, color = "black")+
   geom_smooth(data=subset(tograph_cor, vari_group=="losses_glass"&parm_group=="sMAT"), method="lm", se=F, color = "black")+
   geom_smooth(data=subset(tograph_cor, vari_group=="losses_glass"&parm_group=="sanpp"), method="lm", se=F, color = "black")+
-  geom_text(data=rvalues, mapping=aes(x=Inf, y = Inf, label = r.value), hjust=1.05, vjust=1.5)
+  geom_text(data=rvalues, mapping=aes(x=Inf, y = Inf, label = r.value), hjust=1.05, vjust=1.5)+
+  ggtitle("B)")
+
+grid.arrange(mr, cor, ncol=1, heights=c(1.8,4))
 
 ##-------------glass's D analyses
-
-t.test(change_glass_d_mean$abs_richness_glass, mu=0)#over all sig.
-t.test(change_glass_d_mean$abs_evenness_glass, mu=0)#overall sig.
-t.test(change_glass_d_mean$rank_glass, mu=0)#not sig
-t.test(change_glass_d_mean$gains_glass, mu=0)#not sig
-t.test(change_glass_d_mean$losses_glass, mu=0)#sig.
-
-#water
-irr<-subset(change_glass_d_mean, trt_type2=="Irrigation")
-t.test(irr$abs_richness_glass, mu=0)#not sig
-t.test(irr$abs_evenness_glass, mu=0)#not sig
-t.test(irr$rank_glass, mu=0)#not sig
-t.test(irr$gains_glass, mu=0)#not sig
-t.test(irr$losses_glass, mu=0)#not sig
-
-#N
-N<-subset(change_glass_d_mean, trt_type2=="N")
-t.test(N$abs_richness_glass, mu=0)#sig
-t.test(N$abs_evenness_glass, mu=0)#sig
-t.test(N$rank_glass, mu=0)# sig
-t.test(N$gains_glass, mu=0)# sig
-t.test(N$losses_glass, mu=0)# sig
-
-#P
-P<-subset(change_glass_d_mean, trt_type2=="P")
-t.test(P$abs_richness_glass, mu=0)#not sig
-t.test(P$abs_evenness_glass, mu=0)#not sig
-t.test(P$rank_glass, mu=0)# not sig
-t.test(P$gains_glass, mu=0)# not sig
-t.test(P$losses_glass, mu=0)#not sig
-
-#CO2
-CO2<-subset(change_glass_d_mean, trt_type2=="CO2")
-t.test(CO2$abs_richness_glass, mu=0)#not sig
-t.test(CO2$abs_evenness_glass, mu=0)#not sig
-t.test(CO2$rank_glass, mu=0)# not sig
-t.test(CO2$gains_glass, mu=0)# not sig
-t.test(CO2$losses_glass, mu=0)#not sig
-
-#mult nuts
-multnuts<-subset(change_glass_d_mean, trt_type2=="Mult. Nuts.")
-t.test(multnuts$abs_richness_glass, mu=0)# sig
-t.test(multnuts$abs_evenness_glass, mu=0)# sig
-t.test(multnuts$rank_glass, mu=0)# not sig
-t.test(multnuts$gains_glass, mu=0)# not sig
-t.test(multnuts$losses_glass, mu=0)# sig
-
-#temperature
-temp<-subset(change_glass_d_mean, trt_type2=="Temperature")
-t.test(temp$abs_richness_glass, mu=0)# not sig
-t.test(temp$abs_evenness_glass, mu=0)# not sig
-t.test(temp$rank_glass, mu=0)# not sig
-t.test(temp$gains_glass, mu=0)# not sig
-t.test(temp$losses_glass, mu=0)# not sig
-
-#water and temperature
-irrtemp<-subset(change_glass_d_mean, trt_type2=="Irr + Temp")
-t.test(irrtemp$abs_richness_glass, mu=0)# not sig
-t.test(irrtemp$abs_evenness_glass, mu=0)# not sig
-t.test(irrtemp$rank_glass, mu=0)# not sig
-t.test(irrtemp$gains_glass, mu=0)# not sig
-t.test(irrtemp$losses_glass, mu=0)# not sig
-
-###graphing this.
-glassD_trt<-change_glass_d_mean%>%
-  group_by(trt_type2)%>%
-  summarize(rich=mean(abs_richness_glass),
-            sd_rich=sd(abs_richness_glass),
-            even=mean(abs_evenness_glass),
-            sd_even=sd(abs_evenness_glass),
-            rank=mean(rank_glass),
-            sd_rank=sd(rank_glass),
-            gain=mean(gains_glass),
-            sd_gain=sd(gains_glass),
-            loss=mean(losses_glass),
-            sd_loss=sd(losses_glass),
-            num=length(abs_richness_glass))%>%
-  mutate(se_rich=sd_rich/sqrt(num),
-         se_even=sd_even/sqrt(num),
-         se_rank=sd_rank/sqrt(num),
-         se_gain=sd_gain/sqrt(num),
-         se_loss=sd_loss/sqrt(num))%>%
-  filter(trt_type2=="N"|trt_type2=="Mult. Nuts."|trt_type2=="Irrigation"|trt_type2=="CO2"|trt_type2=="P"|trt_type2=="Temperature"|trt_type2=="Irr + Temp")
-
-glassD_all<-change_glass_d_mean%>%
-  ungroup()%>%
-  summarize(rich=mean(abs_richness_glass),
-            sd_rich=sd(abs_richness_glass),
-            even=mean(abs_evenness_glass),
-            sd_even=sd(abs_evenness_glass),
-            rank=mean(rank_glass),
-            sd_rank=sd(rank_glass),
-            gain=mean(gains_glass),
-            sd_gain=sd(gains_glass),
-            loss=mean(losses_glass),
-            sd_loss=sd(losses_glass),
-            num=length(abs_richness_glass))%>%
-  mutate(se_rich=sd_rich/sqrt(num),
-         se_even=sd_even/sqrt(num),
-         se_rank=sd_rank/sqrt(num),
-         se_gain=sd_gain/sqrt(num),
-         se_loss=sd_loss/sqrt(num))%>%
-  mutate(trt_type2="All Trts")
-
-glassD_bargraph<-rbind(glassD_all, glassD_trt)
-glassD_bargraph_mean<-glassD_bargraph[,c("trt_type2", "rich", "even","rank","gain","loss")]%>%
-  gather(metric, mean, rich:loss)
-
-glassD_bargraph_se<-glassD_bargraph[,c('trt_type2',"se_rich",'se_even','se_rank','se_gain','se_loss')]%>%
-  gather(metric_se, se, se_rich:se_loss)
-
-glassD_toplot<-cbind(glassD_bargraph_mean, glassD_bargraph_se)
-glassD_toplot2<-glassD_toplot[,-4]%>%
-  mutate(sig=ifelse(trt_type2=='All Trts'&metric=="rich", 1, ifelse(trt_type2=='All Trts'&metric=="even", 1, ifelse(trt_type2=='All Trts'&metric=="loss", 1, ifelse(trt_type2=='N', 1, ifelse(trt_type2=='Mult. Nuts.'&metric=="rich", 1, ifelse(trt_type2=='Mult. Nuts'&metric=="even", 1,ifelse(trt_type2=='Mult. Nuts.'&metric=="loss", 1,0))))))))%>%
-  mutate(location=ifelse(sig==1, mean+0.3,NA))%>%
-  mutate(metric_group=factor(metric, levels=c("rich",'even','rank',"gain","loss")))
-
-vari<-c(
-  rich = "Richness Change",
-  even = 'Eveness Change',
-  rank = 'Rank Change',
-  gain = 'Species Gains',
-  loss = 'Species Losses'
-)
-
-ggplot(data=glassD_toplot2, aes(x=trt_type2, y=mean, fill=trt_type2))+
-  geom_bar(position=position_dodge(), stat="identity")+
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se),position= position_dodge(0.9), width=0.2)+
-  ylab("Glass's D")+
-  xlab("")+
-  scale_x_discrete(limits=c("All Trts","CO2","Irrigation","Temperature","Irr + Temp","N","P","Mult. Nuts."), labels=c("All Trts", "CO2","Irrigation", "Temp","Irr + Temp","Nitrogen","Phosphorus","Mult Nuts"))+
-  scale_fill_manual(values=c("black","green3",'purple','blue','darkorange','orange','gold3','red'))+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 0.5))+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none")+
-  geom_vline(xintercept = 1.5, size = 1)+
-  geom_point(aes(trt_type2, location), shape=8)+
-  facet_wrap(~metric_group, ncol=1, labeller = labeller(metric_group=vari))
+# 
+# t.test(change_glass_d_mean$abs_richness_glass, mu=0)#over all sig.
+# t.test(change_glass_d_mean$abs_evenness_glass, mu=0)#overall sig.
+# t.test(change_glass_d_mean$rank_glass, mu=0)#not sig
+# t.test(change_glass_d_mean$gains_glass, mu=0)#not sig
+# t.test(change_glass_d_mean$losses_glass, mu=0)#sig.
+# 
+# #water
+# irr<-subset(change_glass_d_mean, trt_type2=="Irrigation")
+# t.test(irr$abs_richness_glass, mu=0)#not sig
+# t.test(irr$abs_evenness_glass, mu=0)#not sig
+# t.test(irr$rank_glass, mu=0)#not sig
+# t.test(irr$gains_glass, mu=0)#not sig
+# t.test(irr$losses_glass, mu=0)#not sig
+# 
+# #N
+# N<-subset(change_glass_d_mean, trt_type2=="N")
+# t.test(N$abs_richness_glass, mu=0)#sig
+# t.test(N$abs_evenness_glass, mu=0)#sig
+# t.test(N$rank_glass, mu=0)# sig
+# t.test(N$gains_glass, mu=0)# sig
+# t.test(N$losses_glass, mu=0)# sig
+# 
+# #P
+# P<-subset(change_glass_d_mean, trt_type2=="P")
+# t.test(P$abs_richness_glass, mu=0)#not sig
+# t.test(P$abs_evenness_glass, mu=0)#not sig
+# t.test(P$rank_glass, mu=0)# not sig
+# t.test(P$gains_glass, mu=0)# not sig
+# t.test(P$losses_glass, mu=0)#not sig
+# 
+# #CO2
+# CO2<-subset(change_glass_d_mean, trt_type2=="CO2")
+# t.test(CO2$abs_richness_glass, mu=0)#not sig
+# t.test(CO2$abs_evenness_glass, mu=0)#not sig
+# t.test(CO2$rank_glass, mu=0)# not sig
+# t.test(CO2$gains_glass, mu=0)# not sig
+# t.test(CO2$losses_glass, mu=0)#not sig
+# 
+# #mult nuts
+# multnuts<-subset(change_glass_d_mean, trt_type2=="Mult. Nuts.")
+# t.test(multnuts$abs_richness_glass, mu=0)# sig
+# t.test(multnuts$abs_evenness_glass, mu=0)# sig
+# t.test(multnuts$rank_glass, mu=0)# not sig
+# t.test(multnuts$gains_glass, mu=0)# not sig
+# t.test(multnuts$losses_glass, mu=0)# sig
+# 
+# #temperature
+# temp<-subset(change_glass_d_mean, trt_type2=="Temperature")
+# t.test(temp$abs_richness_glass, mu=0)# not sig
+# t.test(temp$abs_evenness_glass, mu=0)# not sig
+# t.test(temp$rank_glass, mu=0)# not sig
+# t.test(temp$gains_glass, mu=0)# not sig
+# t.test(temp$losses_glass, mu=0)# not sig
+# 
+# #water and temperature
+# irrtemp<-subset(change_glass_d_mean, trt_type2=="Irr + Temp")
+# t.test(irrtemp$abs_richness_glass, mu=0)# not sig
+# t.test(irrtemp$abs_evenness_glass, mu=0)# not sig
+# t.test(irrtemp$rank_glass, mu=0)# not sig
+# t.test(irrtemp$gains_glass, mu=0)# not sig
+# t.test(irrtemp$losses_glass, mu=0)# not sig
+# 
+# ###graphing this.
+# glassD_trt<-change_glass_d_mean%>%
+#   group_by(trt_type2)%>%
+#   summarize(rich=mean(abs_richness_glass),
+#             sd_rich=sd(abs_richness_glass),
+#             even=mean(abs_evenness_glass),
+#             sd_even=sd(abs_evenness_glass),
+#             rank=mean(rank_glass),
+#             sd_rank=sd(rank_glass),
+#             gain=mean(gains_glass),
+#             sd_gain=sd(gains_glass),
+#             loss=mean(losses_glass),
+#             sd_loss=sd(losses_glass),
+#             num=length(abs_richness_glass))%>%
+#   mutate(se_rich=sd_rich/sqrt(num),
+#          se_even=sd_even/sqrt(num),
+#          se_rank=sd_rank/sqrt(num),
+#          se_gain=sd_gain/sqrt(num),
+#          se_loss=sd_loss/sqrt(num))%>%
+#   filter(trt_type2=="N"|trt_type2=="Mult. Nuts."|trt_type2=="Irrigation"|trt_type2=="CO2"|trt_type2=="P"|trt_type2=="Temperature"|trt_type2=="Irr + Temp")
+# 
+# glassD_all<-change_glass_d_mean%>%
+#   ungroup()%>%
+#   summarize(rich=mean(abs_richness_glass),
+#             sd_rich=sd(abs_richness_glass),
+#             even=mean(abs_evenness_glass),
+#             sd_even=sd(abs_evenness_glass),
+#             rank=mean(rank_glass),
+#             sd_rank=sd(rank_glass),
+#             gain=mean(gains_glass),
+#             sd_gain=sd(gains_glass),
+#             loss=mean(losses_glass),
+#             sd_loss=sd(losses_glass),
+#             num=length(abs_richness_glass))%>%
+#   mutate(se_rich=sd_rich/sqrt(num),
+#          se_even=sd_even/sqrt(num),
+#          se_rank=sd_rank/sqrt(num),
+#          se_gain=sd_gain/sqrt(num),
+#          se_loss=sd_loss/sqrt(num))%>%
+#   mutate(trt_type2="All Trts")
+# 
+# glassD_bargraph<-rbind(glassD_all, glassD_trt)
+# glassD_bargraph_mean<-glassD_bargraph[,c("trt_type2", "rich", "even","rank","gain","loss")]%>%
+#   gather(metric, mean, rich:loss)
+# 
+# glassD_bargraph_se<-glassD_bargraph[,c('trt_type2',"se_rich",'se_even','se_rank','se_gain','se_loss')]%>%
+#   gather(metric_se, se, se_rich:se_loss)
+# 
+# glassD_toplot<-cbind(glassD_bargraph_mean, glassD_bargraph_se)
+# glassD_toplot2<-glassD_toplot[,-4]%>%
+#   mutate(sig=ifelse(trt_type2=='All Trts'&metric=="rich", 1, ifelse(trt_type2=='All Trts'&metric=="even", 1, ifelse(trt_type2=='All Trts'&metric=="loss", 1, ifelse(trt_type2=='N', 1, ifelse(trt_type2=='Mult. Nuts.'&metric=="rich", 1, ifelse(trt_type2=='Mult. Nuts'&metric=="even", 1,ifelse(trt_type2=='Mult. Nuts.'&metric=="loss", 1,0))))))))%>%
+#   mutate(location=ifelse(sig==1, mean+0.3,NA))%>%
+#   mutate(metric_group=factor(metric, levels=c("rich",'even','rank',"gain","loss")))
+# 
+# vari<-c(
+#   rich = "Richness Change",
+#   even = 'Eveness Change',
+#   rank = 'Rank Change',
+#   gain = 'Species Gains',
+#   loss = 'Species Losses'
+# )
+# 
+# ggplot(data=glassD_toplot2, aes(x=trt_type2, y=mean, fill=trt_type2))+
+#   geom_bar(position=position_dodge(), stat="identity")+
+#   geom_errorbar(aes(ymin=mean-se, ymax=mean+se),position= position_dodge(0.9), width=0.2)+
+#   ylab("Glass's D")+
+#   xlab("")+
+#   scale_x_discrete(limits=c("All Trts","CO2","Irrigation","Temperature","Irr + Temp","N","P","Mult. Nuts."), labels=c("All Trts", "CO2","Irrigation", "Temp","Irr + Temp","Nitrogen","Phosphorus","Mult Nuts"))+
+#   scale_fill_manual(values=c("black","green3",'purple','blue','darkorange','orange','gold3','red'))+
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 0.5))+
+#   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none")+
+#   geom_vline(xintercept = 1.5, size = 1)+
+#   geom_point(aes(trt_type2, location), shape=8)+
+#   facet_wrap(~metric_group, ncol=1, labeller = labeller(metric_group=vari))
